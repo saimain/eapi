9e65b52373f17437d075d6519d6288e885e29fc3
saimain-dev <saimain.dev@gmail.com>
2020-02-23 20:35:26 +0630

    Product not belongs to user

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\ProductNotBelongsToUser;
+use App\Http\Requests\ProductRequest;
+use App\Http\Resources\Product\ProductCollection;
+use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
-use App\Http\Requests\ProductRequest;
+use Auth;
 use Symfony\Component\HttpFoundation\Response;
-use App\Http\Resources\Product\ProductResource;
-use App\Http\Resources\Product\ProductCollection;
 
 class ProductController extends Controller
 {
@@ -88,6 +90,9 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
+
+        $this->ProductUserCheck($product);
+
         $request['detail'] = $request->description;
         unset($request['description']);
         $product->update($request->all());
@@ -106,5 +111,14 @@ class ProductController extends Controller
     {
         $product->delete();
         return response(null, Response::HTTP_NO_CONTENT);
+    }
+
+    public function ProductUserCheck($product)
+    {
+        if (Auth::id() !== $product->user_id) {
+            throw new ProductNotBelongsToUser;
+        }
+
+
     }
 }
